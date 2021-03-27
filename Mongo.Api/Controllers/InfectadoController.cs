15437b9f52234c2d033b57338391f073db8c3486
Victor Hugo <victorhugo-5@hotmail.com>
2021-03-26 21:19:32 -0300

    Adicionar arquivos de projeto.

---
+++ Mongo.Api/Controllers/InfectadoController.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Mongo.Api.Data.Collections;
+using Mongo.Api.Models;
+using MongoDB.Driver;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Mongo.Api.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class InfectadoController : ControllerBase
+    {
+        private readonly Data.MongoDB _mongoDB;
+        private readonly IMongoCollection<Infectado> _infectadosCollection;
+
+        public InfectadoController(Data.MongoDB mongoDB)
+        {
+            _mongoDB = mongoDB;
+            _infectadosCollection = _mongoDB.DB.GetCollection<Infectado>(typeof(Infectado).Name.ToLower());
+        }
+
+        [HttpPost]
+        public ActionResult SalvarInfectado([FromBody] InfectadoDto dto)
+        {
+            var infectado = new Infectado(dto.DataNascimento, dto.Sexo, dto.Latitude, dto.Longitude);
+
+            _infectadosCollection.InsertOne(infectado);
+
+            return StatusCode(201, "Infectado adicionado com sucesso");
+        }
+
+        [HttpGet]
+        public ActionResult ObterInfectados()
+        {
+            var infectados = _infectadosCollection.Find(Builders<Infectado>.Filter.Empty).ToList();
+
+            return Ok(infectados);
+        }
+    }
+}
---
+++ Mongo.Api/Data/Collections/Infectado.cs
@@ -0,0 +1,23 @@
+﻿using MongoDB.Driver.GeoJsonObjectModel;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Mongo.Api.Data.Collections
+{
+    public class Infectado
+    {
+        public DateTime DataNascimento { get; set; }
+        public string Sexo { get; set; }
+        public GeoJson2DGeographicCoordinates Localizacao { get; set; }
+
+        public Infectado(DateTime dataNascimento, string sexo, double latitude, double longitude)
+        {
+            this.DataNascimento = dataNascimento;
+            this.Sexo = sexo;
+            this.Localizacao = new GeoJson2DGeographicCoordinates(longitude, latitude);
+        }
+
+    }
+}
